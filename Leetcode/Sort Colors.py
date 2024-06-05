cf24bd63567760b08ccdac6e8eb7bb602c4026d1
niks-yad <nikhilyadav1.ny@gmail.com>
2024-06-05 06:00:27 +0100

    sort colors

---
+++ Leetcode/Sort Colors.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        def swap(a,b):
+            temp = nums[a]
+            nums[a] = nums[b]
+            nums[b] = temp
+        i = 0
+        l = 0
+        r = len(nums) - 1
+        while i <= r:
+            if nums[i] == 0:
+                swap(l,i)
+                l += 1
+            elif nums[i] == 2:
+                swap(r,i)
+                r -= 1
+                i -= 1
+            i += 1
+
+        return nums
