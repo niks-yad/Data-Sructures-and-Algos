7c23bdfa861d3d16898523fa78b9247207f7fc33
niks-yad <nikhilyadav1.ny@gmail.com>
2024-06-09 11:48:05 +0100

    commit

---
+++ Leetcode/Product of Array Except Self.py
@@ -0,0 +1,19 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        ans = [0] * len(nums)
+        ans[0] = nums[0]
+        # prefix = left to right
+        for i in range(1,len(nums)):
+            ans[i] = nums[i] * ans[i-1]
+
+        # postfix = right to left
+        postfix = 1
+        for i in range(len(nums)-1,0,-1):
+            ans[i] = ans[i-1] * postfix
+            postfix = postfix * nums[i]
+
+        ans[0] = postfix
+
+        return ans
+
+        
